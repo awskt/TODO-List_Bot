cc0ba59c7cdc8dae456ac0f4972e81d61aa75fdd
ADavlatov <a@davlatov.space>
2022-07-25 20:30:19 +0300

    Рабочий код с основными действиями бота из примера

---
+++ TODO-List_Bot/Program.cs
@@ -0,0 +1,33 @@
+using Telegram.Bot;
+using TODO_List_Bot;
+using TODO_List_Bot.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var botConfig = builder.Configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
+
+builder.Services.AddHostedService<ConfigureWebhook>();
+
+builder.Services.AddHttpClient("tgwebhook")
+    .AddTypedClient<ITelegramBotClient>(httpClient => new TelegramBotClient(botConfig.BotToken, httpClient));
+
+builder.Services.AddScoped<HandleUpdateService>();
+
+//из Microsoft.AspNetCore.Mvc.NewtonsoftJson
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseCors();
+
+app.UseEndpoints(endpoints =>
+{
+    var token = botConfig.BotToken;
+    endpoints.MapControllerRoute(name: "tgwebhook",
+                                 pattern: $"bot/{token}",
+                                 new { controller = "Webhook", action = "Post" });
+    endpoints.MapControllers();
+});
+
+app.Run();
