e620be6781d5b9c56b159cb1aba479a13000c891
ADavlatov <a@davlatov.space>
2022-07-26 18:56:38 +0300

    Класс представляющий сам таск

---
+++ TODO-List_Bot/TaskObject.cs
@@ -0,0 +1,12 @@
+namespace TODO_List_Bot;
+
+public class TaskObject
+{
+    public TaskObject(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+    //public string Decription { get; set; }
+}
